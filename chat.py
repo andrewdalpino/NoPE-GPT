Fix SFT label masking

chat.py
@@ -3,19 +3,19 @@ import random
 from os import path
 from functools import partial
 from argparse import ArgumentParser
+from itertools import chain
 
 import torch
 
 from torch.cuda import is_available as cuda_is_available
 
 from model import LightGPT
-from data import CHATML_TEMPLATE
+from data import CHATML_TEMPLATE, RESPONSE_HEADER
 from memory import ChatMemory
 
 DEFAULT_SYSTEM_MESSAGE = (
-    "You're a helpful AI assistant named LightGPT."
-    "Your job is to answer the user's queries as accurately as possible."
-    "If you are unsure of an answer, just say 'I don't know'."
+    "You're a helpful AI assistant named LightGPT. "
+    "Your job is to chat and answer questions as accurately as possible. "
 )
 
 
@@ -30,7 +30,7 @@ def main():
     )
     parser.add_argument("--max_tokens", default=500, type=int)
     parser.add_argument("--context_length", default=1024, type=int)
-    parser.add_argument("--temperature", default=1.0, type=float)
+    parser.add_argument("--temperature", default=0.7, type=float)
     parser.add_argument("--top_k", default=500, type=int)
     parser.add_argument("--top_p", default=0.9, type=float)
     parser.add_argument("--device", default="cuda", type=str)
@@ -65,6 +65,11 @@ def main():
 
     tokenizer = checkpoint["tokenizer"]
 
+    im_end_token = tokenizer.encode_single_token("<|im_end|>")
+    eot_token = tokenizer.eot_token
+
+    stop_tokens = frozenset({im_end_token, eot_token})
+
     model.resize_token_embeddings(tokenizer.n_vocab)
     model.token_embeddings.load_state_dict(checkpoint["token_embeddings"])
 
@@ -87,6 +92,10 @@ def main():
 
     system_message_tokens = tokenizer.encode(system_message, allowed_special="all")
 
+    response_header_tokens = tokenizer.encode(RESPONSE_HEADER, allowed_special="all")
+
+    newline_token = tokenizer.encode_single_token("\n")
+
     max_message_history_length = args.context_length - len(system_message_tokens)
 
     memory = ChatMemory(max_message_history_length)
@@ -105,31 +114,34 @@ def main():
 
         instruction = CHATML_TEMPLATE.format(role="user", message=instruction)
 
-        instruction += "<|im_start|>assistant\n"
-
         instruction_tokens = tokenizer.encode(instruction, allowed_special="all")
+
+        prompt = chain(
+            system_message_tokens,
+            memory.get_history(),
+            instruction_tokens,
+            response_header_tokens,
+        )
+
+        prompt = torch.tensor(list(prompt), dtype=torch.int64, device=args.device)
 
         memory.add_message(instruction_tokens)
 
-        prompt = system_message_tokens
-
-        prompt.extend(memory.get_history())
-
-        prompt = torch.tensor(prompt, dtype=torch.int64, device=args.device)
-
-        message_tokens = []
+        message_tokens = response_header_tokens
 
         for token in generate(prompt):
+            message_tokens.append(token)
+
+            if token in stop_tokens:
+                break
+
             out = tokenizer.decode_single_token_bytes(token).decode(
                 "utf-8", errors="replace"
             )
 
             print(out, end="", flush=True)
 
-            message_tokens.append(token)
-
-            if token == tokenizer.eot_token:
-                break
+        message_tokens.append(newline_token)
 
         print("\n")
 

data.py
@@ -2,6 +2,7 @@ import random
 
 from os import path, remove as delete_file
 from functools import partial
+from copy import deepcopy
 
 from datasets import load_dataset
 
@@ -17,17 +18,17 @@ from torch.nn.utils.rnn import pad_sequence
 
 from tqdm import tqdm
 
-
 CHATML_TEMPLATE = "<|im_start|>{role}\n{message}\n<|im_end|>\n"
 
-PADDING_INDEX = 0
+RESPONSE_HEADER = "<|im_start|>assistant\n"
+
 IGNORE_INDEX = -100
 
 
 class Fineweb(IterableDataset):
     """
-    The Fineweb dataset by HuggingFace, tokenized and stored as a memory map for fast
-    access during pre-training.
+    The Fineweb dataset by HuggingFace, tokenized, and stored as a memory map for fast access
+    during pre-training. Endlessly emits random chunks of tokens from the corpus in infinitum.
     """
 
     DATASET_NAME = "HuggingFaceFW/fineweb"
@@ -153,8 +154,8 @@ class Fineweb(IterableDataset):
             start += self.tokens_per_sample
 
 
-class ChatMLMixin:
-    """Mixin class for datasets that use the ChatML message format."""
+class ChatMLTokenizer:
+    """Tokenizer for multi-turn ChatML-formatted messages."""
 
     def __init__(
         self,
@@ -162,22 +163,28 @@ class ChatMLMixin:
         max_tokens_per_sample: int = 1024,
         train_on_inputs: bool = False,
     ):
-        super().__init__()
-
         if max_tokens_per_sample < 1:
             raise ValueError(
                 f"Max tokens per sample must be greater than 0, {max_tokens_per_sample} given."
             )
 
+        response_header_tokens = tokenizer.encode(
+            RESPONSE_HEADER, allowed_special="all"
+        )
+        response_header_length = len(response_header_tokens)
+
         self.tokenizer = tokenizer
         self.max_tokens_per_sample = max_tokens_per_sample
         self.train_on_inputs = train_on_inputs
+        self.response_header_length = response_header_length
 
     def tokenize_messages(self, messages: list[dict]) -> tuple[Tensor, Tensor]:
-        sample, label = [], []
+        sample, labels = [], []
+
+        total_tokens = 0
 
         for message in messages:
-            is_end_of_turn = message["role"] == "assistant"
+            is_completion = message["role"] == "assistant"
 
             text = CHATML_TEMPLATE.format(
                 role=message["role"],
@@ -186,31 +193,40 @@ class ChatMLMixin:
 
             tokens = self.tokenizer.encode(text, allowed_special="all")
 
-            if is_end_of_turn:
-                tokens.append(self.tokenizer.eot_token)
+            total_tokens += len(tokens)
 
-            sample.extend(tokens[:-1])
+            sample.extend(tokens)
 
-            if is_end_of_turn or self.train_on_inputs:
-                label.extend(tokens[1:])
+            if self.train_on_inputs:
+                labels.extend(tokens)
+
+                continue
+
+            if is_completion:
+                labels.extend([IGNORE_INDEX] * self.response_header_length)
+
+                labels.extend(tokens[self.response_header_length :])
             else:
-                label.extend([IGNORE_INDEX] * (len(tokens) - 1))
+                labels.extend([IGNORE_INDEX] * len(tokens))
 
-            if len(sample) >= self.max_tokens_per_sample:
+            if total_tokens > self.max_tokens_per_sample:
                 break
 
+        sample = sample[:-1]
+        labels = labels[1:]
+
         sample = sample[: self.max_tokens_per_sample]
-        label = label[: self.max_tokens_per_sample]
+        labels = labels[: self.max_tokens_per_sample]
 
         x = torch.tensor(sample, dtype=torch.int64)
-        y = torch.tensor(label, dtype=torch.int64)
+        y = torch.tensor(labels, dtype=torch.int64)
 
         assert x.shape == y.shape, "Sample / label shape mismatch"
 
         return x, y
 
 
-class SmolTalk(ChatMLMixin, Dataset):
+class SmolTalk(Dataset):
     """
     The SmolTalk dataset by HuggingFace formatted as ChatML messages for supervised instruction tuning.
     """
@@ -238,22 +254,22 @@ class SmolTalk(ChatMLMixin, Dataset):
 
     def __init__(
         self,
-        tokenizer: Encoding,
+        tokenizer: ChatMLTokenizer,
         subset: str = "all",
-        max_tokens_per_sample: int = 1024,
-        train_on_inputs: bool = False,
     ):
-        super().__init__(tokenizer, max_tokens_per_sample, train_on_inputs)
+        super().__init__()
 
         if subset not in self.SUBSETS:
             raise ValueError(f"Invalid subset, {subset} given.")
+
+        self.tokenizer = tokenizer
 
         self.dataset = load_dataset(self.DATASET_NAME, subset, split="train")
 
     def __getitem__(self, index: int):
         messages = self.dataset[index]["messages"]
 
-        x, y = self.tokenize_messages(messages)
+        x, y = self.tokenizer.tokenize_messages(messages)
 
         return x, y
 
@@ -261,7 +277,7 @@ class SmolTalk(ChatMLMixin, Dataset):
         return len(self.dataset)
 
 
-class UltraFeedback(ChatMLMixin, Dataset):
+class UltraFeedbackSFT(Dataset):
     """
     The binarized version of the UltraFeedback dataset for human preference alignment via SFT.
     """
@@ -270,15 +286,15 @@ class UltraFeedback(ChatMLMixin, Dataset):
 
     def __init__(
         self,
-        tokenizer: Encoding,
+        tokenizer: ChatMLTokenizer,
         split: str = "train",
-        max_tokens_per_sample: int = 1024,
-        train_on_inputs: bool = False,
     ):
-        super().__init__(tokenizer, max_tokens_per_sample, train_on_inputs)
+        super().__init__()
 
         if split not in {"train", "test"}:
             raise ValueError(f"Split must be either train or test, {split} given.")
+
+        self.tokenizer = tokenizer
 
         new_dataset = partial(load_dataset, name=self.DATASET_NAME)
 
@@ -290,7 +306,7 @@ class UltraFeedback(ChatMLMixin, Dataset):
     def __getitem__(self, index: int):
         messages = self.dataset[index]["messages"]
 
-        x, y = self.tokenize_messages(messages)
+        x, y = self.tokenizer.tokenize_messages(messages)
 
         return x, y
 
@@ -298,23 +314,25 @@ class UltraFeedback(ChatMLMixin, Dataset):
         return len(self.dataset)
 
 
-def left_pad_collate(batch: list[tuple[Tensor, Tensor]]) -> tuple[Tensor, Tensor]:
-    """Custom collate function adds left padding to batched samples."""
+def pad_collate(
+    batch: list[tuple[Tensor, Tensor]], padding_side: str, padding_index: int
+) -> tuple[Tensor, Tensor]:
+    """Custom collate function adds padding to batched samples."""
 
     samples, labels = zip(*batch)
 
     x = pad_sequence(
         list(samples),
         batch_first=True,
-        padding_value=PADDING_INDEX,
-        padding_side="left",
+        padding_value=padding_index,
+        padding_side=padding_side,
     )
 
     y = pad_sequence(
         list(labels),
         batch_first=True,
         padding_value=IGNORE_INDEX,
-        padding_side="left",
+        padding_side=padding_side,
     )
 
     assert x.shape == y.shape, "Batch shape mismatch"

generate.py
@@ -68,7 +68,7 @@ def main():
     while True:
         prompt = input("Enter a prompt: ")
 
-        prompt = tokenizer.encode_ordinary(prompt)
+        prompt = tokenizer.encode(prompt, allowed_special="all")
 
         prompt = torch.tensor(prompt, dtype=torch.int64, device=args.device)
 

instruction-tune.py
@@ -16,7 +16,7 @@ from torch.utils.tensorboard import SummaryWriter
 from torchmetrics.text import Perplexity
 
 from model import LightGPT
-from data import SmolTalk, UltraFeedback, left_pad_collate, IGNORE_INDEX
+from data import ChatMLTokenizer, SmolTalk, UltraFeedbackSFT, pad_collate, IGNORE_INDEX
 
 from tiktoken import Encoding
 
@@ -36,7 +36,7 @@ def main():
     parser.add_argument(
         "--dataset_subsets", default=["all", "ultra-feedback"], type=csv_list
     )
-    parser.add_argument("--num_dataset_processes", default=2, type=int)
+    parser.add_argument("--num_dataset_processes", default=1, type=int)
     parser.add_argument("--max_tokens_per_sample", default=1048, type=int)
     parser.add_argument("--train_on_inputs", action="store_true")
     parser.add_argument("--batch_size", default=2, type=int)
@@ -44,13 +44,14 @@ def main():
     parser.add_argument("--learning_rate", default=5e-4, type=float)
     parser.add_argument("--rms_decay", default=-0.8, type=float)
     parser.add_argument("--low_memory_optimizer", action="store_true")
-    parser.add_argument("--max_gradient_norm", default=1.0, type=float)
+    parser.add_argument("--max_gradient_norm", default=10.0, type=float)
     parser.add_argument("--num_epochs", default=3, type=int)
     parser.add_argument("--rank", default=8, type=int)
     parser.add_argument("--alpha", default=1.0, type=float)
     parser.add_argument("--dropout", default=0.05, type=float)
     parser.add_argument("--activation_checkpointing", action="store_true")
     parser.add_argument("--eval_interval", default=1, type=int)
+    parser.add_argument("--eval_ratio", default=0.1, type=float)
     parser.add_argument("--checkpoint_interval", default=1, type=int)
     parser.add_argument(
         "--checkpoint_path", default="./checkpoints/instruct.pt", type=str
@@ -88,6 +89,11 @@ def main():
     if args.eval_interval < 1:
         raise ValueError(
             f"Eval interval must be greater than 0, {args.eval_interval} given."
+        )
+
+    if args.eval_ratio < 0 or args.eval_ratio > 1:
+        raise ValueError(
+            f"Eval ratio must be between 0 and 1, {args.eval_ratio} given."
         )
 
     if args.checkpoint_interval < 1:
@@ -134,45 +140,42 @@ def main():
         },
     )
 
+    chatml_tokenizer = ChatMLTokenizer(
+        tokenizer,
+        max_tokens_per_sample=args.max_tokens_per_sample,
+        train_on_inputs=args.train_on_inputs,
+    )
+
     datasets = []
 
     for subset in frozenset(args.dataset_subsets):
         if subset in SmolTalk.SUBSETS:
-            datasets.append(
-                SmolTalk(
-                    tokenizer,
-                    subset=subset,
-                    max_tokens_per_sample=args.max_tokens_per_sample,
-                    train_on_inputs=args.train_on_inputs,
-                )
-            )
+            datasets.append(SmolTalk(chatml_tokenizer, subset=subset))
 
         if subset == "ultra-feedback":
-            datasets.append(
-                UltraFeedback(
-                    tokenizer,
-                    split="train",
-                    max_tokens_per_sample=args.max_tokens_per_sample,
-                    train_on_inputs=args.train_on_inputs,
-                )
-            )
+            datasets.append(UltraFeedbackSFT(chatml_tokenizer, split="train"))
 
     dataset = ConcatDataset(datasets)
 
-    training, testing, _ = random_split(dataset, (0.009, 0.001, 0.99))
+    training, testing = random_split(dataset, (1.0 - args.eval_ratio, args.eval_ratio))
+
+    right_pad_collate = partial(
+        pad_collate, padding_side="right", padding_index=tokenizer.eot_token
+    )
 
     train_loader = DataLoader(
         training,
-        collate_fn=left_pad_collate,
+        collate_fn=right_pad_collate,
         batch_size=args.batch_size,
         pin_memory="cpu" not in args.device,
         shuffle=True,
         num_workers=args.num_dataset_processes,
+        drop_last=True,
     )
 
     test_loader = DataLoader(
         testing,
-        collate_fn=left_pad_collate,
+        collate_fn=right_pad_collate,
         batch_size=args.batch_size,
         pin_memory="cpu" not in args.device,
         shuffle=False,
@@ -182,9 +185,6 @@ def main():
     model_args = checkpoint["model_args"]
 
     model = LightGPT(**model_args)
-
-    if args.activation_checkpointing:
-        model.enable_activation_checkpointing()
 
     state_dict = checkpoint["model"]
 
@@ -207,6 +207,9 @@ def main():
     }
 
     model.add_lora_parameters(**lora_args)
+
+    if args.activation_checkpointing:
+        model.enable_activation_checkpointing()
 
     model = model.to(args.device)
 
@@ -252,7 +255,7 @@ def main():
             y = y.to(args.device, non_blocking=True)
 
             with amp_context:
-                y_pred, loss = model(x, y)
+                y_pred, loss = model.forward(x, y)
 
                 scaled_loss = loss / args.gradient_accumulation_steps
 

model.py
@@ -99,12 +99,6 @@ class LightGPT(Module):
         for param in self.parameters():
             param.requires_grad = False
 
-    def unfreeze_token_embeddings(self) -> None:
-        """Unfreeze the token embeddings to allow for fine-tuning."""
-
-        for param in self.token_embeddings.parameters():
-            param.requires_grad = True
-
     @torch.no_grad()
     def resize_token_embeddings(self, vocabulary_size: int) -> None:
         """Resize the token embeddings to accommodate a new vocabulary size."""
@@ -137,8 +131,13 @@ class LightGPT(Module):
 
         self.vocabulary_size = vocabulary_size
 
+    def unfreeze_token_embeddings(self) -> None:
+        """Unfreeze the token embeddings to allow for fine-tuning."""
+
+        self.token_embeddings.weight.requires_grad = True
+
     def add_lora_parameters(self, rank: int, alpha: float, dropout: float) -> None:
-        """Reparameterize the weights of the model using LoRA."""
+        """Reparameterize the weights of the model using LoRA adapters."""
 
         for module in self.body:
             register_parametrization(